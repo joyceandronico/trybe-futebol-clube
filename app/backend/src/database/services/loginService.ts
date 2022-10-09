04148710e72cbe3174d93cecd19f218c5f460928
joyceandronicco <joyceandronico@msn.com>
2022-10-09 02:41:02 -0300

    fix: :rotating_light: corrige método de encriptação de senha

--- app/backend/src/database/services/loginService.ts
+++ app/backend/src/database/services/loginService.ts
@@ -1,19 +1,32 @@
 import * as bcrypt from 'bcryptjs';
 import Users from '../models/users';
 import { IUser } from '../interfaces/userInterface';
-import createToken from '../helpers/jwtAuth';
+import createToken, { decoder } from '../helpers/jwtAuth';
 
 export default class User {
   private model = Users;
 
   newLogin = async (email: string, password: string) => {
     const findUser = await this.model.findOne({ where: { email } }) as IUser;
-    if (!findUser) throw new Error('user validation');
+    const correctPassword = bcrypt.hashSync(password, findUser.password as string);
 
-    const correctPassword = bcrypt.compareSync(password, findUser.password as string);
-    if (!correctPassword) throw new Error('user validation');
+    if (!correctPassword) {
+      throw new Error('incorrect password');
+    }
+
+    if (!findUser) {
+      throw new Error('incorrect email');
+    }
 
     const token = await createToken(email, findUser.role);
     return { token };
+  };
+
+  loginValidate = async (token: string | undefined) => {
+    if (typeof token === 'string') {
+      const findUser = decoder(token);
+      return { role: findUser };
+    }
+    return { error: 'Erro' };
   };
 }
