cfa59668c9640eda057a5922d4832ffec58aa03e
joyceandronicco <joyceandronico@msn.com>
2022-10-09 02:44:22 -0300

    feat: :white_check_mark: cria teste para a rota /login/validate

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -12,21 +12,25 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-//import users from '../database/models/users';
+import users from '../database/models/users';
 
 const mockUser = ({
-
-  email: 'test@test.com',
-  password:'senha_mock'
-
-
+  id: 1,
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password:'$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
 });
+
+const mockLogin = ({
+email: 'teste@teste.com',
+password:'$2a$08$xi.Hxk1czAO0nZRB393u10aED0RQ1N3PA',})
 
 describe('/login', () => {
 
   describe('Verifica que não é possível fazer login com email ou senha incorretos', () => {
     it('Verifica rota /login', async () => {
-      const response = await (chai.request(app).post('/login')).send(mockUser);
+      const response = await (chai.request(app).post('/login')).send( mockLogin);
       expect(response.status).to.be.equal(401);
       expect (response.body).to.deep.equal({
         "message": "Incorrect email or password"
@@ -54,9 +58,31 @@ describe('/login', () => {
     });
   });
 
+});
 
+describe('/login/validate', () => {
 
-  /**
+  before(async () => {
+    sinon
+      .stub(users, "findOne")
+      .resolves(mockUser as users);
+    });
+
+  after(()=>{
+    (users.findOne as sinon.SinonStub).restore();
+  })
+
+  it('Verifica se o objeto retornado contém a informação role:user', async () => {
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY2NTI5Mjk2NywiZXhwIjoxNjY1Mzc5MzY3fQ.O1HB_t_0g2xVGFiNvB4AUOTKsBXBwTNvvSNJ6poYzA4'
+    const response = await chai
+    .request(app).get('/login/validate')
+       .set('authorization', token)
+       expect(response.status).to.be.equal(200);
+       expect(response.body).to.deep.equal({role: 'admin'});
+        });
+      })
+
+      /**
    * Exemplo do uso de stubs com tipos
    */
 
@@ -85,4 +111,4 @@ describe('/login', () => {
   // it('Seu sub-teste', () => {
   //   expect(false).to.be.eq(true);
   // });
-});
+
