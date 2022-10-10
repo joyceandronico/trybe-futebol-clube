18d1043d0b69f3da8f5158762ae05e818e129923
joyceandronicco <joyceandronico@msn.com>
2022-10-10 05:20:41 -0300

    feat: :sparkles: cria validação de partidas de forma que não seja possível inserir uma partida com times iguais

--- app/backend/src/database/routes/matches.routes.ts
+++ app/backend/src/database/routes/matches.routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 
 import MatchesController from '../controllers/matchesController';
 import validTokenMiddleware from '../middlewares/validToken.middleware';
+import validMatch from '../middlewares/validMatch.middleware';
 
 const matchesRouter = Router();
 
 const matchesController = new MatchesController();
 
 matchesRouter.get('/', matchesController.getAllMatches);
-matchesRouter.post('/', validTokenMiddleware, matchesController.createNewMatch);
+matchesRouter.post('/', validTokenMiddleware, validMatch, matchesController.createNewMatch);
 matchesRouter.patch('/:id/finish', matchesController.finishMatch);
 
 export default matchesRouter;
